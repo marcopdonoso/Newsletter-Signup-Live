b09e491f76c0e527f10869b2f80d484a8d60f2b6
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-02-08 14:03:04 -0400

    PORT const created

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const request = require("request");
 const https = require("https");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -62,8 +63,8 @@ app.post("/failure", function(req, res) {
 });
 
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log("Server is running on port 3000.")
+app.listen(PORT, function() {
+    console.log("Server is running on port ${PORT}")
 });
 
 //Mailchimp API Key
