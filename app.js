ad0fdf618d2c5f24cd1f1d92cf299696d2b9c718
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-02-08 14:05:28 -0400

    PORT const created

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post("/failure", function(req, res) {
 
 
 app.listen(PORT, function() {
-    console.log("Server is running on port ${PORT}")
+    console.log('Server is running on port ' + PORT);
 });
 
 //Mailchimp API Key
